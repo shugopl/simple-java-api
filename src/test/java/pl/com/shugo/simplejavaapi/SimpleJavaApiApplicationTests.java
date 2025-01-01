1ec58d22d6db5536334361e21906eba05685a94b
Maciej Góra <maciej.gora@softwaremind.com>
2025-01-01 14:44:37 +0100

    Init commit

---
+++ src/test/java/pl/com/shugo/simplejavaapi/SimpleJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.shugo.simplejavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
